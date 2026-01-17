931c54ec8d6c8151a915add4b4943385c5dd90f9
Triple K <triplek.dev@gmail.com>
2026-01-17 22:42:50 +0100

    WIP

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
 console.log('Hello World!')
+
+const shareSection = document.querySelector(".share-links");
+const activeShareIcon = document.querySelector(".share-icon-active");
+const shareIcon = document.getElementById("share-icon");
+const authorSection = document.getElementById("author-section");
+
+function share() {
+	// Tab to edit
+	if (screen.width>= 600) {
+		showShareSection()
+		// toggleIconColor()
+	} else if(screen.width < 600) {
+		changeSections()
+	} else {
+		return
+	}
+}
+
+function changeSections() {
+	// Tab to edit
+	shareSection.style.display = 'flex';
+	authorSection.style.display = 'none';
+}
+
+function showShareSection() {
+	// Tab to edit
+	shareSection.style.display = (shareSection.style.display === 'none') ? 'flex' : 'none';
+	authorSection.style.display = 'flex';
+}
